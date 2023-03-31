dcb4a1effdc60e36d1e2b3dfc63e3371d22d0c35
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-03-31 01:03:02 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *rot13 - encodes a string using rot13
+ *@str: string as parameter
+ *Return: alwyas result
+ */
+
+char *rot13(char *str)
+{
+	char *result = str;
+	int i;
+	int j;
+	char *alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == alph[j])
+			{
+				result[i] = rot13[j];
+				break;
+			}
+
+		}
+	}
+	return (result);
+}
