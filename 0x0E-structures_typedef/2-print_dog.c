957d4a5cada69838f65cf658718c5a8548276a8a
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-04-18 17:13:17 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ *print_dog -  a function that prints a struct dogi
+ *@d: the poimter
+ */
+
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
