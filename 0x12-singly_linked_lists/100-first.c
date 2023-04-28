669338a5be517b38dc406efb251f8d7a4e998d10
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-04-27 20:18:03 -0700

    fun tortise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ *tortoise_hare - Apply the constructor attribute
+ *Return: 0
+ */
+
+void tortoise_hare(void)
+{
+	printf("You're beat and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
