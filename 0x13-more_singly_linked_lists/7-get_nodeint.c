ce8f3c3d40f8461b85133d1541d850bfdb997725
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-05-02 03:52:30 -0700

    function that returns the nth

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,19 +9,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int a = 0;
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-	while (a > index)
+	while (a < index)
 	{
 		head = head->next;
+		a++;
 		if (head == NULL)
 		{
 			return (NULL);
-
 		}
-		a++;
 	}
 	return (head);
 }
