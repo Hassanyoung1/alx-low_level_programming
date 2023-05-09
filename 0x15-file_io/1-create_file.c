06c7ae2a81272e7adaf86c99e35408620b29b3e7
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-05-08 19:52:27 -0700

    function to create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,22 +9,24 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int len;
+	mode_t mode = S_IRUSR | S_IWUSR;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd == -1)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fd < 0)
 	{
 		return (-1);
 	}
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++)
+		for (len = 0; text_content[len] != '\0'; len++)
 		{
 			if (write(fd, text_content, len) != len)
 			{
+				close(fd);
 				return (-1);
 			}
 		}
