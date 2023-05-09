dc33325a2b92174c6d1463b281dbea3ad09130be
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-05-08 20:31:34 -0700

    append to file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,28 +9,20 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int len;
-	mode_t mode = S_IRUSR | S_IWUSR;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
-	if (fd < 0)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
 	{
 		return (-1);
 	}
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len] != '\0'; len++)
-		{
-			if (write(fd, text_content, len) != len)
-			{
-				close(fd);
-				return (-1);
-			}
-		}
+		len = write(fd, text_content, strlen(text_content));
 	}
 	close(fd);
-	return (1);
+	return (len == -1 ? -1 : 1);
 }
