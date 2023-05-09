bdbac6da439e328c44df46aea64a9401b13dbd0a
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-05-08 19:23:44 -0700

    function to create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *create_file - function that creates a file.
+ *@filename: name of the file
+ *@text_content: content of the file
+ *Return: -1 if it fails
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (!text_content)
+	{
+		for (len = 0; text_content[len]; len++)
+		{
+			if (write(fd, text_content, len) != len)
+			{
+				return (-1);
+			}
+		}
+	}
+	close(fd);
+	return (1);
+}
