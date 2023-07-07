f6b02764a22fd4c6f0c2156d0603cba06e5f7038
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-07-07 16:22:43 -0700

    create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Function to create the hash table
+ * @size: the size of the hash table
+ * Return: pointer to the newly created hash table
+**/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table = NULL;
+	unsigned long int i;
+
+	/** Allocate memory top the hash table**/
+	hash_table = malloc(sizeof(hash_table_t));
+
+	if (hash_table == NULL)
+	{
+		return (NULL);
+	}
+	/** Declare the  Linglist of an hashtable **/
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	/** Loop through the table **/
+	for (i = 0; i < size; i++)
+	{
+		hash_table->array[i] = NULL;
+	}
+/**	hash_table->array = size;**/
+	return (hash_table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+This is a project on Hash table (0x1A. C - Hash tables)
