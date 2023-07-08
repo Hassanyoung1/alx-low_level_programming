2a5806b2b472142f45bd2d57e30caa6554d9ff6e
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-07-07 19:45:12 -0700

    key index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,7 +10,7 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash_index; 
+	unsigned long int hash_index;
 	int sum_ascii;
 
 	sum_ascii = hash_djb2(key);
