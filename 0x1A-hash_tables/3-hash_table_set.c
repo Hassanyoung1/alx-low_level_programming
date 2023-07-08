13e6d84812802b9fc9405162f31600e7bbdd5cb0
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-07-07 21:28:04 -0700

    aDD TO THE ASH TABLE

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,54 +1,45 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - sets value for a given hashtable
- * @ht: The hashtable
- * @key: Key to determine index to input value in table
- * @value: Given value to input in table
- * Return: 1 is successful, 0 otherwise
+ * hash_table_set - Sets the value for a given hash table.
+ * @ht: The hash table.
+ * @key: The key to determine the index to input the value in the table.
+ * @value: The given value to input in the table.
+ *
+ * Return: 1 if successful, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node = NULL;
-	hash_node_t *current_node = NULL;
+	hash_node_t *new_node, *current_node;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
-	/* Check if the key already exists in the hash table */
 	current_node = ht->array[index];
-	while (current_node != NULL)
+
+	while (current_node)
 	{
 		if (strcmp(current_node->key, key) == 0)
 		{
-			/* Update the value for the existing key */
 			free(current_node->value);
 			current_node->value = strdup(value);
-			if (current_node->value == NULL)
-				return (0);
 			return (1);
 		}
 		current_node = current_node->next;
 	}
 
-	/* Create a new node */
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-		return (0);
-
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	if (new_node->key == NULL || new_node->value == NULL)
 	{
-		free(new_node->key);
-		free(new_node->value);
 		free(new_node);
 		return (0);
 	}
-/* Add the new node at the beginning of the list */
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -2,7 +2,8 @@
 #define HASH_TABLES_H
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stddef.h>
+#include <string.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
