3303268d1c40e85852c05d28af047d4627654ecd
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-09-10 14:31:53 -0700

    gives you the index of a key.

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - returns the index of a key in the hash table array
+ * @key: the key to be hashed
+ * @size: the size of the hash table array
+ *
+ * Return: the index at which the key/value pair should be stored
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    unsigned long int hash_value;
+
+    /* Use the hash_djb2 function to compute the hash value */
+    hash_value = hash_djb2(key);
+
+    /* Calculate the index by taking the remainder of hash_value and size */
+    return (hash_value % size);
+}
